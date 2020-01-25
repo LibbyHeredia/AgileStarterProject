d27a9c32a749e2193d0717bd690b5a5b7f822f9e
brendan <bdivney@nd.edu>
2020-01-24 20:12:23 -0500

    quiz framework done, need to fetch questions and handle answers

--- starter-project/src/app/quiz/quiz.component.ts
+++ starter-project/src/app/quiz/quiz.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { QuizService } from '../../shared/quiz.service';
+import { AngularFirestore} from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-quiz',
+  selector: 'app-Quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent implements OnInit {
+export class quizComponent implements OnInit {
 
-  constructor() { }
+  emailPattern = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$";
+
+  constructor( private service: QuizService,
+    private router : Router,
+    private firestore : AngularFirestore,
+    private toastr : ToastrService  ) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form? : NgForm){
+    if(form != null)
+      form.resetForm();
+    this.service.formData ={
+      answers : {},
+      user : '',
+      email : '',
+      password : '',
+      score : null
+    }
+  }
+
+  onSubmit(form : NgForm){
+    console.log("Submitted");
+    let data = form.value;
+
+    //insert into firestore DB collection
+    this.firestore.collection("users").add(data)
+    this.resetForm(form);
+    this.navigateToLogin();
+    this.toastr.success("Submitted succesfully", 'Quiz Register')
+  }
+
+  navigateToLogin(){
+    this.router.navigate(['./login'])
   }
 
 }
